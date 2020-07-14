814824c5e6d2352916e14989f61e1479f3659d46
Duc <vanduc2514@gmail.com>
2020-07-14 09:01:07 +0700

    Add route for book list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
-import { BookListComponent } from './book-list/book-list.component';
+import {AppComponent} from './app.component';
+import {BookComponent} from './book/book.component';
+import {BookListComponent} from './book-list/book-list.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
-    BookListComponent
+    BookListComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
